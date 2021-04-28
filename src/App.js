673e6de4767ca96354498355538ff08c60ec5c99
Kazeem Erinfolami <erinfolamibolaji2010@gmail.com>
2021-04-28 08:21:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import React from "react"
-import { useSpring, animated } from 'react-spring'
-import Nav from './components/Nav';
-import CV from './components/CV';
-import DateTime from "./CurrentTimendDate";
-import profileImg from "../src/img/IMG_4234.jpg"
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './Routes/Routes';
 
 function App() {
-  const props = useSpring({
-    from: { opacity: 0, marginTop: -500 },
-    to: { opacity: 1, marginTop: 100 },
-    config: { delay: 1000, duration: 2000 }
-  })
-  const text = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    config: { delay: 1000, duration: 5000 }
-  })
   return (
-    <>
-      <Nav />
-      <DateTime />
-      <CV />
-      <animated.div style={props} className="homeContainer">
-        < div className="profImgContainer">
-          <img className="profImg" src={profileImg} />
-        </div>
-        <animated.div style={text}>
-          <div>
-            <span className="devIntro">Looking for a Developer! 👨🏽‍💻</span>
-          </div>
-          <div className="name1">
-            < span className="logoName">Howdy, i'm</span> <span className="name">Kazeem Erinfolami </span>
-          </div>
-          <div className="name2">
-            <span className="jobTitle">i'm a freelance Web Designer & Developer</span><span className="smileEmoji">🖥️</span>
-          </div>
-        </animated.div>
-      </animated.div>
-    </>
+    <Router>
+      <Routes />
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-import Routes from './Routes';
-import Nav from './components/Nav';
 
-ReactDOM.render(<div><Routes /></div>, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
